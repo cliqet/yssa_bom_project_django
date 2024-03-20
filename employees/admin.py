5dcc0ce0ea2972645f9e790d5b953a54d8869f65
Mike Torres <michael@aqwire.co>
2024-03-20 18:24:42 +0700

    filter users excluding superuser

--- employees/admin.py
+++ employees/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import Department, EmployeePosition, Employee
 
 
 LIST_PAGE_COUNT = 20
 
 
-class EmployeeAdmin(admin.ModelAdmin):
+class EmployeeAdmin(BaseUserAdmin):
     list_display = (
         'id', 'email', 'first_name', 'last_name', 'contact_no', 'department', 'employee_position',
         'is_active', 'is_staff', 'is_superuser',
@@ -15,6 +16,30 @@ class EmployeeAdmin(admin.ModelAdmin):
     list_filter = ['department', 'employee_position', 'is_active', 'is_staff', 'is_superuser']
     search_fields = ['email', 'first_name', 'last_name']
     list_per_page = LIST_PAGE_COUNT
+    fieldsets = (
+        ('User Credentials', {'fields': ('email', 'password')}),
+        ('User Information', {'fields': (
+            'first_name',
+            'last_name',
+            'contact_no',
+            'department',
+            'employee_position'
+        )}),
+        ('Permissions', {'fields': (
+            'is_active',
+            'is_staff',
+            'is_superuser',
+            'groups',
+            'user_permissions'
+        )}),
+    )
+
+    # do not show superuser in list of users if current user is not a superuser
+    def get_queryset(self, request):
+        qs = super(EmployeeAdmin, self).get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(is_superuser=False)
 
 
 admin.site.register(Department)
