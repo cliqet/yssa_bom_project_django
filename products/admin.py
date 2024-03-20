b668382334c33ef4317c61381274e1156b19de6c
Mike Torres <michael@aqwire.co>
2024-03-20 08:46:04 +0700

    customize admin for products

--- products/admin.py
+++ products/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
 from .models import Product
 
-admin.site.register(Product)
+
+LIST_PAGE_COUNT = 20
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'setup_type', 'name',)
+    ordering = ('id',)
+    list_display_links = ['name']
+    list_filter = ['setup_type', 'name']
+    list_per_page = LIST_PAGE_COUNT
+
+
+admin.site.register(Product, ProductAdmin)
