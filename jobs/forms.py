f237965952675b5de17dd442775cc57040eec12f
Mike Torres <michael@aqwire.co>
2024-03-25 22:09:39 +0700

    limit options for dropdown

--- jobs/forms.py
+++ jobs/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.db.models import Q
 from employees.models import Employee
 from .models import Job
 
@@ -12,4 +13,17 @@ class JobForm(forms.ModelForm):
         # Limit the choices for the sales_executive field
         self.fields['sales_executive'].queryset = Employee.objects.filter(
             employee_position__employee_position_title='Sales Executive'
+        )
+        # Limit the choices for the prepared_by field
+        self.fields['prepared_by'].queryset = Employee.objects.filter(
+            employee_position__employee_position_title='Project Officer'
+        )
+        # Limit the choices for the reviewed_by field
+        self.fields['reviewed_by'].queryset = Employee.objects.filter(
+            employee_position__employee_position_title='Sales Executive'
+        )
+        # Limit the choices for the approved_by field
+        self.fields['approved_by'].queryset = Employee.objects.filter(
+            Q(employee_position__employee_position_title='Sales Executive') |
+            Q(employee_position__employee_position_title='Purchasing')
         )
