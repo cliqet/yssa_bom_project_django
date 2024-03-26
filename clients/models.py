d949e6e220940f6c64b81bef83db6fc51b78a69c
Mike Torres <michael@aqwire.co>
2024-03-26 19:19:03 +0700

    make client name unique

--- clients/models.py
+++ clients/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Client(models.Model):
-    company_name = models.CharField(max_length=255)
+    company_name = models.CharField(max_length=255, unique=True)
     contact_person = models.CharField(max_length=255)
     contact_no = models.CharField(max_length=50, blank=True, null=True)
     email_address = models.EmailField(max_length=100, blank=True, null=True)
